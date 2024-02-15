9b9c2ebcf585d3632cf749b638a8c02b5e37b0d9
dduneon <dduneon@gmail.com>
2024-02-15 10:15:11 +0900

    [FEAT] Route Config 등록 및 Properties class 등록

---
+++ src/main/java/store/ckin/gateway/config/RouteConfig.java
@@ -0,0 +1,26 @@
+package store.ckin.gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import store.ckin.gateway.properties.ServerInfoProperties;
+
+@Configuration
+@RequiredArgsConstructor
+public class RouteConfig {
+    private final ServerInfoProperties serverInfoProperties;
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("ckin-auth", r -> r.path("/auth/**")
+                        .uri(serverInfoProperties.getAuthUri()))
+                .route("ckin-coupon", r -> r.path("/coupon/**")
+                        .uri(serverInfoProperties.getCouponUri()))
+                .route("ckin-api", r -> r.path("/api/**")
+                        .uri(serverInfoProperties.getApiUri()))
+                .build();
+    }
+}
