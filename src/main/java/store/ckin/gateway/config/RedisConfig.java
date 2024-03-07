5b6134610ce3df41b10f6757ab3b200e73879b92
jinwoo Lee <jinwoolee@jinwooui-noteubug.local>
2024-03-07 09:40:32 +0900

    [FEAT] RedisConfig

---
+++ src/main/java/store/ckin/gateway/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package store.ckin.gateway.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis 관련 설정을 하는 클래스 입니다.
+ *
+ * @author : jinwoolee
+ * @version : 2024. 03. 07.
+ */
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisConfig {
+    private String host;
+
+    private int port;
+
+    private String password;
+
+    private int database;
+
+    /**
+     * Redis connection factory redis connection factory.
+     *
+     * @return the redis connection factory
+     */
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(password);
+        config.setDatabase(database);
+
+        return new LettuceConnectionFactory(config);
+    }
+
+    /**
+     * JWT 관리를 위한 Redis Template 를 Bean 으로 등록한 클래스 입니다.
+     *
+     * @return RedisTemplate
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
